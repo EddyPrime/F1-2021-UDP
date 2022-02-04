f85a40dd603e368a3f62268aff7f1e54e45a9a10
EddyPrime <edoardo.liberati27@gmail.com>
2022-02-04 18:40:35 +0100

    Trying to connect from another device

--- app/app.c
+++ app/app.c
@@ -13,11 +13,13 @@ int main(int argc, char **argv)
     ssize_t res;
     struct PacketHeader packetHeader;
     int sockfd;
+    unsigned servaddrlen;
     struct sockaddr_in servaddr;
 
-    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    servaddr.sin_addr.s_addr = inet_addr(SERVER_ADDRESS);
     servaddr.sin_port = htons(PORT);
     servaddr.sin_family = AF_INET;
+    servaddrlen = sizeof(servaddr);
 
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     if (sockfd < 0)
@@ -25,21 +27,29 @@ int main(int argc, char **argv)
         perror("socket()");
         exit(1);
     }
+    printf("opened socket with fd %d\n", sockfd);
 
-    if (connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0) 
+    if (connect(sockfd, (struct sockaddr *)&servaddr, servaddrlen) < 0)
     {
         perror("connect()");
         exit(1);
     }
+    printf("connected to server\n");
 
-    res = recvfrom(sockfd, &packetHeader, sizeof(struct PacketHeader), 0,(struct sockaddr*)NULL, NULL);
-    if (res < 0) {
-        perror("recvfrom()");
-        exit(1);
+    char buf[2000];
+
+    while (1)
+    {
+        printf("trying to receive\n");
+        res = recvfrom(sockfd, &packetHeader, sizeof(struct PacketHeader), 0, (struct sockaddr *)&servaddr, &servaddrlen);
+        if (res < 0)
+        {
+            perror("recvfrom()");
+            exit(1);
+        }
+        printf("retrieved %ld bytes\n", res);
     }
-    
-    printf("retrieved %ld bytes\n", res);
+
     close(sockfd);
     exit(0);
-
 }
--- app/app.h
+++ app/app.h
@@ -1,3 +1,4 @@
 #include "../packets/header.h"
 
+#define SERVER_ADDRESS "192.168.1.254"
 #define PORT 20777
